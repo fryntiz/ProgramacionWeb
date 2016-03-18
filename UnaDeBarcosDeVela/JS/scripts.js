79a0eb6df8c0bc3804725820e1d8a57000791f58
fryntiz <fryntiz@gmail.com>
2016-03-18 19:21:37 +0100

    Función para flecha interactiva que sube al principio de la página

---
+++ UnaDeBarcosDeVela/JS/scripts.js
@@ -0,0 +1,14 @@
+/*
+------------------------------------------
+Flecha para subir
+------------------------------------------
+*/
+function mostrarSubirPagina() {
+	var flechasubir = document.getElementById("flechasubir");
+	flechasubir.style.display = "block";
+}
+
+function noMostrarSubirPagina() {
+	var flechasubir = document.getElementById("flechasubir");
+	flechasubir.style.display = "none";
+}
