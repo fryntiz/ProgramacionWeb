6d32552d06954cd35bb873be194db97c03d9e711
fryntiz <fryntiz@gmail.com>
2016-03-21 19:27:53 +0100

    Base para procesar contacto

--- UnaDeBarcosDeVela/procesarcontacto.php
+++ UnaDeBarcosDeVela/procesarcontacto.php
@@ -1,33 +1,60 @@
 <?php
-// Preferencias
-$para = "raupulus@gmail.com";
-$asunto = "Contacto desde el sitio web";
+header('Content-Type: text/html; charset=UTF-8');
 
-	//Variables generales
+error_reporting( E_ALL & ~( E_NOTICE | E_STRICT | E_DEPRECATED ) ); //Aquí se genera un control de errores "NO BORRAR NI SUSTITUIR"
+require_once "Mail.php"; //Aquí se llama a la función mail "NO BORRAR NI SUSTITUIR"
+
+$to = 'DESTINO@gmail.com'; //Aquí definimos quien recibirá el formulario
+$from = 'informacion@nzweb.org'; //Aquí definimos que cuenta mandará el correo, generalmente perteneciente al mismo dominio
+$host = 'smtp.nzweb.org'; //Aquí definimos cual es el servidor de correo saliente desde el que se enviaran los correos
+$username = 'informacion@nzweb.org'; //Aqui se define el usuario de la cuenta de correo, en hostalia separado con punto en vez de @
+$password = 'PASSWORD'; //Aquí se define la contraseña de la cuenta de correo que enviará el mensaje
+$subject = 'Contacto desde la página web'; //Aquí se define el asunto del correo
+$port = '587'; //Definido el puerto que utilizará
+
+//Datos importados mediante POST
 $nombre = $_POST['nombre'];
 $apellidos = $_POST['apellidos'];
-$mail = $_POST['mail'];
+$email = $_POST['email'];
 $telefono = $_POST['telefono'];
 $comentario = $_POST['comentario'];
 
-// Validación de datos
-
-//Preparamos el mensaje que contendrá el correo
+//Aquí se define el cuerpo de correo
 $mensaje = "Detalles del formulario de contacto:\n\n";
 $mensaje .= "Nombre: " . $nombre . "\n";
 $mensaje .= "Apellidos: " . $apellidos . "\n";
-$mensaje .= "Correo electrónico: " . $mail . "\n";
+$mensaje .= "Correo electrónico: " . $email . "\n";
 $mensaje .= "Teléfono: " . $telefono . "\n";
 $mensaje .= "Comentario: " . $comentario . "\n\n";
 
+//A partir de aquí empleamos la función mail para enviar el formulario
 
-// Ahora se envía el e-mail usando la función mail() de PHP
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
-mail($para, $asunto, $mensaje, $header);
+$headers = array ('From' => $from,
+'To' => $to,
+'Subject' => $subject);
+$smtp = Mail::factory('smtp',
+array ('host' => $host,
+'auth' => true,
+'username' => $username,
+'password' => $password,
+'port' => $port,
+'SMTPAuth' => true,
+'SMTPSecure' => "tsl"));
 
-echo "¡El formulario se ha enviado con éxito!";
+$mail = $smtp->send($to, $headers, $mensaje);
+
+//Una vez aquí habremos enviado el mensaje mediante el formulario
+
+//El siguiente codigo muestra en pantalla un mensaje indicando que el mensaje ha sido enviado y a que cuenta ES OPCIONAL desde Acens lo incluimos para verificar que el formulario de prueba esta funcionando
+
+if (PEAR::isError($mail)) {
+echo("
+
+" . $mail->getMessage() . "
+");
+} else {
+echo "Mensaje enviado desde POA a ". $to ;
 }
+
+
 ?>
