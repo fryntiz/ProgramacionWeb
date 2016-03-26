3e4ca59a69b31eedad3ff0393bb2b094b518842a
fryntiz <fryntiz@gmail.com>
2016-03-26 19:38:25 +0100

    Reestructurado menú de redes sociales y menú fijo

--- DesdeChipiona/menu-superior-fixed.php
+++ DesdeChipiona/menu-superior-fixed.php
@@ -1,12 +1,32 @@
-		<DIV id="desplegarmenu">Menú</DIV>
-		<DIV id="desplegarporque">Redes Sociales</DIV>
+		<DIV id="desplegarMenu" onClick="EventoMenu()">Menú
+			<DIV id="menuDesplegado">
+				<UL>
+					<A href="#"><LI>Rutas Turísticas Locales</LI></A>
+					<A href="#"><LI>Webs Amigas</LI></A>
+					<A href="#"><LI>Contacto</LI></A>
+					<A href="#"><LI>Sobre este proyecto</LI></A>
+					<A href="#"><LI>Desarrollador WEB</LI></A>
+				</UL>
+			</DIV>
+		</DIV>
+
+		<DIV id="desplegarRedes" onClick="EventoRedes()">Redes<BR/>Sociales
+			<DIV id="redesDesplegado">
+				<UL>
+					<LI id="RedesCorreo" class="BotonMenu"></LI>
+					<LI id="RedesFacebook" class="BotonMenu"></LI>
+					<LI id="RedesTwitter" class="BotonMenu"></LI>
+					<LI id="RedesYoutube" class="BotonMenu"></LI>
+				</UL>
+			</DIV>
+		</DIV>
 
 		<DIV id="cajamenusuperior">
 			<NAV id="menusuperior">
 				<UL>
-					<LI><A href="#"><P><IMG src="./images/botones/volver_inicio.png"/></P>Inicio</A></LI>
-					<LI><A href="#"><P><IMG src="./images/botones/videos.png"/></P>Vídeos</A></LI>
-					<LI><A href="#"><P><IMG src="./images/botones/about.png"/></P>Sobre Chipiona</A></LI>
+					<LI><A href="index.php">Inicio</A></LI>
+					<LI><A href="#">Vídeos</A></LI>
+					<LI><A href="#">Sobre Chipiona</A></LI>
 				</UL>
 			</NAV>
 		</DIV>
