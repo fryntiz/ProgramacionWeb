69f2e5e6241c2f8567fd6eedaef00c4fead9d43e
fryntiz <fryntiz@gmail.com>
2016-03-27 19:32:44 +0200

    Agrupado en funciones con condiciones para modificar la ruta de la galería actual. Añadidas variables para JS con el directorio de cada galería

--- DesdeChipiona/carrusel.php
+++ DesdeChipiona/carrusel.php
@@ -3,33 +3,52 @@
 	<UL id="ulselectorimagen"> <!--Con PHP traigo las imágenes por lotes del directorio y proceso con JS-->
 		<?php
 		header('Content-Type: text/html; charset=UTF-8');
-
-		$directorioImagenes1 = "images/1_chipiona_general/thumbnail";
-		$directorioActualImagenes = dir($directorioImagenes1);
 		$longitudArrayImagenes = "0";
-		$prueba = "probando";
+		$directorioImagenes = "images/1_chipiona_general/thumbnail";
+		$NumeroGaleria = "1";
 
-		$directory="./images/1_chipiona_general/thumbnail";
-    $dirint = dir($directory);
-		while (($archivo = $directorioActualImagenes->read()) !== false)
+		function directorioPrincipal()
 		{
-			if (eregi("gif", $archivo) || eregi("jpg", $archivo) || eregi("png", $archivo)){
-				$longitudArrayImagenes++;
-				echo '<LI><IMG class="selectorimagen" onclick="cambiarBackground('.$longitudArrayImagenes.')" src="'.$directorioImagenes1."/".$archivo.'"></LI>'."\n";
+			global $directorioImagenes;
+			global $longitudArrayImagenes;
+			global $NumeroGaleria;
+			$arrayImagenes = dir($directorioImagenes);
+			while (($archivo = $arrayImagenes->read()) !== false)
+			{
+				if (eregi("gif", $archivo) || eregi("jpg", $archivo) || eregi("png", $archivo)){
+					$longitudArrayImagenes++;
+					echo '<LI><IMG class="selectorimagen" onclick="cambiarBackground('."$longitudArrayImagenes, $NumeroGaleria".')" src="'.$directorioImagenes."/".$archivo.'"></LI>'."\n";
+				}
 			}
-    }
-    $dirint->close();
+			$arrayImagenes->close();
+		}
 
-		//echo "<SCRIPT>"."var longitudArrayImagenes = ".$longitudArrayImagenes."</SCRIPT>";
+		function cambiardirectorio() {
+			global $directorioImagenes;
+			if ($NumeroGaleria == 1) {
+				$directorioImagenes = "images/1_chipiona_general/thumbnail";
+			} else if ($NumeroGaleria == 2) {
+				$directorioImagenes = "images/2_playas/thumbnail";
+			} else if ($NumeroGaleria == 3) {
+				$directorioImagenes = "images/3_monumentos/thumbnail";
+			} else if ($NumeroGaleria == 4) {
+				$directorioImagenes = "images/4_flora/thumbnail";
+			} else if ($NumeroGaleria == 5) {
+				$directorioImagenes = "images/5_fauna/thumbnail";
+			}
+			echo directorioPrincipal();
+		}
+
+		cambiardirectorio();
+
 		echo "<SCRIPT>
 						longitudArrayImagenes = $longitudArrayImagenes;
+						var General = 'images/1_chipiona_general/high/';
+						var Playas = 'images/2_playas/high/';
+						var Monumentos = 'images/3_monumentos/high/';
+						var Flora = 'images/4_flora/high/';
+						var Fauna = 'images/5_fauna/high/';
 					</SCRIPT>";
-		/*
-		echo "<SCRIPT>
-						longitudArrayImagenes = $longitudArrayImagenes;
-						directorioActualImagenes = 'No consigo que funcione la variable para la ruta actual de imágenes';
-					</SCRIPT>";
-		*/
 	?>
 	</UL>
 </DIV>
@@ -61,5 +80,3 @@
 
 			<DIV id="carruselinferior"></DIV>
 		</DIV>
-
-
