d191ad620e51f44a9040f8bc021bd75c37407cf2
fryntiz <fryntiz@gmail.com>
2016-05-09 20:39:12 +0200

    Corregida distribución,  proporciones, visibilidad y colores para el menú fijo superior

--- DesdeChipiona/menu-superior-fixed.php
+++ DesdeChipiona/menu-superior-fixed.php
@@ -1,26 +1,3 @@
-		<DIV id="desplegarMenu" onClick="EventoMenu()">Menú
-			<DIV id="menuDesplegado">
-				<UL>
-					<A href="#"><LI>Rutas Turísticas Locales</LI></A>
-					<A href="#"><LI>Webs Amigas</LI></A>
-					<A href="#"><LI>Contacto</LI></A>
-					<A href="#"><LI>Sobre este proyecto</LI></A>
-					<A href="#"><LI>Desarrollador WEB</LI></A>
-				</UL>
-			</DIV>
-		</DIV>
-
-		<DIV id="desplegarRedes" onClick="EventoRedes()">Redes<BR/>Sociales
-			<DIV id="redesDesplegado">
-				<UL>
-					<LI id="RedesCorreo" class="BotonMenu"></LI>
-					<LI id="RedesFacebook" class="BotonMenu"></LI>
-					<LI id="RedesTwitter" class="BotonMenu"></LI>
-					<LI id="RedesYoutube" class="BotonMenu"></LI>
-				</UL>
-			</DIV>
-		</DIV>
-
 		<DIV id="cajamenusuperior">
 			<NAV id="menusuperior">
 				<UL>
@@ -30,3 +7,27 @@
 				</UL>
 			</NAV>
 		</DIV>
+
+<DIV id="cajaCentrarMenus">
+	<DIV id="desplegarMenu" onClick="EventoMenu()">Menú</DIV>
+	<DIV id="menuDesplegado">
+		<UL>
+			<A href="#"><LI>Rutas Turísticas Locales</LI></A>
+			<A href="#"><LI>Webs Amigas</LI></A>
+			<A href="#"><LI>Contacto</LI></A>
+			<A href="#"><LI>Sobre este proyecto</LI></A>
+			<A href="#"><LI>Desarrollador WEB</LI></A>
+		</UL>
+	</DIV>
+
+	<DIV id="desplegarRedes" onClick="EventoRedes()">Redes<BR/>Sociales
+		<DIV id="redesDesplegado">
+			<UL>
+				<LI id="RedesCorreo" class="BotonMenu"></LI>
+				<LI id="RedesFacebook" class="BotonMenu"></LI>
+				<LI id="RedesTwitter" class="BotonMenu"></LI>
+				<LI id="RedesYoutube" class="BotonMenu"></LI>
+			</UL>
+		</DIV>
+	</DIV>
+</DIV>
