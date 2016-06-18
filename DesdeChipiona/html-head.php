29b1dda313e693d0393e6c419d4c83a57f000bfe
fryntiz <fryntiz@gmail.com>
2016-06-18 18:20:16 +0200

    Añadido código de seguimiento para google analytics

--- DesdeChipiona/html-head.php
+++ DesdeChipiona/html-head.php
@@ -13,3 +13,12 @@
 		<SCRIPT src="JS/carrusel.js"></SCRIPT>
 		<SCRIPT src="JS/scripts.js"></SCRIPT>
 		<SCRIPT src="JS/jquery.js"></SCRIPT>
+		<SCRIPT>//Google Analytics código de seguimiento
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-79546771-1', 'auto');
+			ga('send', 'pageview');
+		</SCRIPT>
