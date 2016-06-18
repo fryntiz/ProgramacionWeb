485b37e69ab7d25b7b6cce62fc2e43f9f5f9b674
fryntiz <fryntiz@gmail.com>
2016-06-18 18:22:03 +0200

    identificador para títulos

--- DesdeChipiona/titulo-y-logo.php
+++ DesdeChipiona/titulo-y-logo.php
@@ -1,5 +1,5 @@
 		<HEADER id="titulos">
-			<H1>DesdeChipiona</H1>
+			<H1 id="tituloPrincipal">DesdeChipiona</H1>
 			<P>Página web de fotografías</P>
 			<H2>Respira el aire puro....</H2>
 			<IMG src="images/titulo.png"/>
