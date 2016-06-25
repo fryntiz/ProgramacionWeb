a8d460e068c16caf7df968af6dfb70f67016a84b
fryntiz <fryntiz@gmail.com>
2016-06-25 23:11:38 +0200

    incluido head

---
+++ UnaDeBarcosDeVela/head.php
@@ -0,0 +1,15 @@
+   <HEAD>
+		<TITLE>Barcos de Vela</TITLE>
+		<META charset="utf-8"/>
+		<LINK rel="shortcut icon" href="./images/favicon.png"/>
+		<LINK rel="stylesheet" href="./CSS/estilos.css"/>
+		<LINK rel="stylesheet" href="./CSS/1000.css"/>
+		<SCRIPT src="./JS/scripts.js"></SCRIPT>
+		<SCRIPT src="./JS/slideprincipal.js"></SCRIPT>
+<!--Script para avanzar SLIDE cada 8 segundos-->
+		<SCRIPT>
+			window.onload = function () {
+				setInterval(autoCambiarSlide, 8000);
+			}
+		</SCRIPT>
+	</HEAD>
