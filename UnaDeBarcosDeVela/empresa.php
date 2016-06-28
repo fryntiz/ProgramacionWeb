9eab46367b3b87390c0588bc95ca4ce8bb7d9fbd
fryntiz <fryntiz@gmail.com>
2016-06-28 16:08:43 +0200

    Creada base para empresa

---
+++ UnaDeBarcosDeVela/empresa.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<?php
+	include 'head.php';
+?>
+
+	<BODY id="inicio">
+		<?php
+			include 'menuSuperior.php';
+		?>
+
+		<DIV id="cajageneral">
+			<DIV id="cajacontenido">
+				<SECTION>
+					<HEADER>
+						<H2>Titulo</H2>
+					</HEADER>
+
+					<ARTICLE>
+						Contenido
+					</ARTICLE>
+				</SECTION>
+			</DIV>
+		</DIV>
+
+		<?php
+			include 'pieDePagina.php'
+		?>
+
+	</BODY>
+</HTML>
