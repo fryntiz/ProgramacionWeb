5e281d296576f87910ad874e4c53eed6dab3767b
fryntiz <fryntiz@gmail.com>
2016-06-28 16:08:02 +0200

    Renombrado a PHP

---
+++ UnaDeBarcosDeVela/embarcaciones_de_vela.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<?php
+	include 'head.php';
+?>
+
+	<BODY id="embarcaciones_de_vela">
+		<?php
+			include 'menuSuperior.php';
+		?>
+
+<!--BARCOS:-->
+		<DIV id="cajageneral">
+			<?php
+				include 'lateral.php';
+			?>
+
+			<DIV id="cajacontenido">
+				<H2>EMBARCACIONES DE VELA</H2>
+				<SECTION id="barcos">
+<!--BARCO 1-->
+						<DIV class="cajabarcos">
+							<DIV><H3>Nombre del barco 1</H3></DIV>
+							<DIV class="barcoimagen"><IMG src="images/embarcaciones_de_vela/Barco1/1.jpg" alt="vela1"/></DIV>
+							<DIV class="barcocontenido">
+								<UL>
+									<LI>Este barco es una maravilla Este barco es una maravilla Este barco es una maravilla Este barco es una maravilla </LI>
+									<LI>Este barco es una maravilla</LI>
+									<LI>Este barco es una maravilla</LI>
+									<LI>Este barco es una maravilla</LI>
+									<LI>Este barco es una maravilla</LI>
+								</UL>
+							</DIV>
+							<TABLE class="barcostablas">
+								<TR><!--Español-->
+									<TD><IMG src="images/es.png" alt="Español"/></TD>
+									<TD>ESLORA</TD>
+									<TD>MANGA</TD>
+									<TD>PUNTAL</TD>
+									<TD>PESO</TD>
+								</TR>
+								<TR><!--Inglés-->
+									<TD><IMG src="images/en.png" alt="Inglés"/></TD>
+									<TD>LENGTH</TD>
+									<TD>WIDTH</TD>
+									<TD>DEPTH</TD>
+									<TD>WEIGHT</TD>
+								</TR>
+								<TR><!--Alemán-->
+									<TD><IMG src="images/de.png" alt="Alemán"/></TD>
+									<TD>LÄNGE</TD>
+									<TD>BREITE</TD>
+									<TD>TIEFE</TD>
+									<TD>GEWICHT</TD>
+								</TR>
+								<TR><!--Francés-->
+									<TD><IMG src="images/fr.png" alt="Francés"/></TD>
+									<TD>LONGUEUR</TD>
+									<TD>LARGER</TD>
+									<TD>PROFONDEUR</TD>
+									<TD>POIDS</TD>
+								</TR>
+								<TR><!--Holandés-->
+									<TD><IMG src="images/nl.png" alt="Holandés"/></TD>
+									<TD>LENGTE</TD>
+									<TD>BREEDTE</TD>
+									<TD>DIEPTE</TD>
+									<TD>GEWICHT</TD>
+								</TR>
+								<TR class="resultado"><!--Datos-->
+									<TD></TD>
+									<TD>2,51 M.</TD>
+									<TD>1,23 M.</TD>
+									<TD>0.75 M.</TD>
+									<TD>129 KG</TD>
+								</TR>
+							</TABLE>
+
+							<DIV>
+								Ver más información sobre este barco
+								<BR/>
+								AQUÍ UN ENLACE a la página del barco
+							</DIV>
+						</DIV><!--Fin de Barco 1-->
+
+<!--BARCO 2-->
+						<DIV class="cajabarcos">
+							<DIV><H3>Nombre del barco 2</H3></DIV>
+							<DIV class="barcoimagen"><IMG src="images/embarcaciones_de_vela/Barco2/1.jpg" alt="vela2"/></DIV>
+							<DIV class="barcocontenido">
+								<UL>
+									<LI>Este barco es una maravilla Este barco es una maravilla Este barco es una maravilla Este barco es una maravilla </LI>
+									<LI>Este barco es una maravilla</LI>
+									<LI>Este barco es una maravilla</LI>
+									<LI>Este barco es una maravilla</LI>
+									<LI>Este barco es una maravilla</LI>
+								</UL>
+							</DIV>
+							<TABLE class="barcostablas">
+								<TR><!--Español-->
+									<TD><IMG src="images/es.png" alt="Español"/></TD>
+									<TD>ESLORA</TD>
+									<TD>MANGA</TD>
+									<TD>PUNTAL</TD>
+									<TD>PESO</TD>
+								</TR>
+								<TR><!--Inglés-->
+									<TD><IMG src="images/en.png" alt="Inglés"/></TD>
+									<TD>LENGTH</TD>
+									<TD>WIDTH</TD>
+									<TD>DEPTH</TD>
+									<TD>WEIGHT</TD>
+								</TR>
+								<TR><!--Alemán-->
+									<TD><IMG src="images/de.png" alt="Alemán"/></TD>
+									<TD>LÄNGE</TD>
+									<TD>BREITE</TD>
+									<TD>TIEFE</TD>
+									<TD>GEWICHT</TD>
+								</TR>
+								<TR><!--Francés-->
+									<TD><IMG src="images/fr.png" alt="Francés"/></TD>
+									<TD>LONGUEUR</TD>
+									<TD>LARGER</TD>
+									<TD>PROFONDEUR</TD>
+									<TD>POIDS</TD>
+								</TR>
+								<TR><!--Holandés-->
+									<TD><IMG src="images/nl.png" alt="Holandés"/></TD>
+									<TD>LENGTE</TD>
+									<TD>BREEDTE</TD>
+									<TD>DIEPTE</TD>
+									<TD>GEWICHT</TD>
+								</TR>
+								<TR class="resultado"><!--Datos-->
+									<TD></TD>
+									<TD>2,51 M.</TD>
+									<TD>1,23 M.</TD>
+									<TD>0.75 M.</TD>
+									<TD>129 KG</TD>
+								</TR>
+							</TABLE>
+
+							<DIV>
+								Ver más información sobre este barco
+								<BR/>
+								AQUÍ UN ENLACE a la página del barco
+							</DIV>
+						</DIV><!--Fin de Barco 2-->
+
+<!--BARCO 3-->
+						<DIV class="cajabarcos">
+							<DIV><H3>Nombre del barco 3</H3></DIV>
+							<DIV class="barcoimagen"><IMG src="images/embarcaciones_de_vela/Barco3/1.jpg" alt="vela3"/></DIV>
+							<DIV class="barcocontenido">
+								<UL>
+									<LI>Este barco es una maravilla Este barco es una maravilla Este barco es una maravilla Este barco es una maravilla </LI>
+									<LI>Este barco es una maravilla</LI>
+									<LI>Este barco es una maravilla</LI>
+									<LI>Este barco es una maravilla</LI>
+									<LI>Este barco es una maravilla</LI>
+								</UL>
+							</DIV>
+							<TABLE class="barcostablas">
+								<TR><!--Español-->
+									<TD><IMG src="images/es.png" alt="Español"/></TD>
+									<TD>ESLORA</TD>
+									<TD>MANGA</TD>
+									<TD>PUNTAL</TD>
+									<TD>PESO</TD>
+								</TR>
+								<TR><!--Inglés-->
+									<TD><IMG src="images/en.png" alt="Inglés"/></TD>
+									<TD>LENGTH</TD>
+									<TD>WIDTH</TD>
+									<TD>DEPTH</TD>
+									<TD>WEIGHT</TD>
+								</TR>
+								<TR><!--Alemán-->
+									<TD><IMG src="images/de.png" alt="Alemán"/></TD>
+									<TD>LÄNGE</TD>
+									<TD>BREITE</TD>
+									<TD>TIEFE</TD>
+									<TD>GEWICHT</TD>
+								</TR>
+								<TR><!--Francés-->
+									<TD><IMG src="images/fr.png" alt="Francés"/></TD>
+									<TD>LONGUEUR</TD>
+									<TD>LARGER</TD>
+									<TD>PROFONDEUR</TD>
+									<TD>POIDS</TD>
+								</TR>
+								<TR><!--Holandés-->
+									<TD><IMG src="images/nl.png" alt="Holandés"/></TD>
+									<TD>LENGTE</TD>
+									<TD>BREEDTE</TD>
+									<TD>DIEPTE</TD>
+									<TD>GEWICHT</TD>
+								</TR>
+								<TR class="resultado"><!--Datos-->
+									<TD></TD>
+									<TD>2,51 M.</TD>
+									<TD>1,23 M.</TD>
+									<TD>0.75 M.</TD>
+									<TD>129 KG</TD>
+								</TR>
+							</TABLE>
+
+							<DIV>
+								Ver más información sobre este barco
+								<BR/>
+								AQUÍ UN ENLACE a la página del barco
+							</DIV>
+						</DIV><!--Fin de Barco 3-->
+					</SECTION>
+			</DIV>
+		</DIV>
+
+		<?php
+			include 'pieDePagina.php';
+		?>
+	</BODY>
+</HTML>
