19fa7cac7141e68d123574fa2c9b0c54ae50805e
fryntiz <fryntiz@gmail.com>
2016-06-28 16:11:14 +0200

    Separado lateral para llamarlo mediante include

---
+++ UnaDeBarcosDeVela/lateral.php
@@ -0,0 +1,40 @@
+<ASIDE id="cajamenuizquierda">
+				<H4>DESTACADO</H4>
+				Caja menú izquierda 200px
+				<TABLE>
+					<TR>
+						<TD>
+							Nombre del Barco
+							<DIV id="imgIzquierda1" class="IMGBackground"></DIV>
+							<BR/>
+							<P>
+								Descripcion breve con un enlace que sugiera entrar a la página para visitarlo terminando con un enlace así: para <A href="#">mas información pulsa aquí</A>
+							</P>
+						</TD>
+					</TR>
+
+					<TR>
+						<TD>
+							IMAGEN 140px x 100px
+							<BR/>
+							Descripcion
+						</TD>
+					</TR>
+
+					<TR>
+						<TD>
+							IMAGEN 140px x 100px
+							<BR/>
+							Descripcion
+						</TD>
+					</TR>
+
+					<TR>
+						<TD>
+							IMAGEN 140px x 100px
+							<BR/>
+							Descripcion
+						</TD>
+					</TR>
+				</TABLE>
+			</ASIDE>
