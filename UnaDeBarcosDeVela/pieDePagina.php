4d910701886e254c6a3cf22db7e8c04136bd88b8
fryntiz <fryntiz@gmail.com>
2016-06-28 16:12:21 +0200

    Separado pie de página para llamarlo mediante 'include'

---
+++ UnaDeBarcosDeVela/pieDePagina.php
@@ -0,0 +1,14 @@
+		<DIV id="piedepagina">
+			Correo: sdfgvdfsfgsdfg@gmail.com
+			<BR/>
+			Teléfono: 123123123
+			<BR/>
+			Derechos de autor
+		</DIV>
+
+		<DIV id="cajaflechasubir">
+			<A href="#titulo">
+				<DIV id="flechasubir"></DIV>
+				<P onmouseOver="mostrarSubirPagina()" onmouseout="noMostrarSubirPagina()">Subir Página</P>
+			</A>
+		</DIV>
